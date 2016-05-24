34d9fa47ed906cdfa9565103191a0a29528da444
aswathyd <aswathyd@gmail.com>
2016-05-24 10:49:20 -0400

    express

--- node_js/app.js
+++ node_js/app.js
@@ -2,7 +2,20 @@ var express = require('express')//gives refrence to express
 
 var app = express(); //instance of espress
 
-var port = 5000; //port that express listen on you michen
+var port = 5000; //port that express listen on you machine
+
+//set the public directory as static
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+//route
+app.get('/',function(req,res){
+        res.send('hello world')
+        });
+
+app.get('/books',function(req,res){
+        res.send('hello Books')
+        });
 app.listen(port,function(err){
            console.log('runing port' + port);
            });
