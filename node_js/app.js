4e4ce97dcdb45e12a91f655455e1820eb026733e
aswathyd <aswathyd@gmail.com>
2016-06-03 16:55:27 -0400

    routing

--- node_js/app.js
+++ node_js/app.js
@@ -3,16 +3,51 @@ var express = require('express'); //gives refrence to express
 var app = express(); //instance of espress
 
 var port = process.env.PORT || 5000; //port that express listen on you machine
+var bookRouter = express.Router();
 
 //set the public directory as static
 app.use(express.static('public'));
 app.set('views', './src/views');
 
-
-
 app.set('view engine','ejs');
 
+var books = [
+    {
+    title: 'war and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+    },{
+    title: 'war and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+    },  {
+    title: 'war and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+},{
+    title: 'war and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+}];
+bookRouter.route('/')
+    .get(function(req,res){
+    res.render('books',{title:'Books',
+                        nav: [{
+                            Link:'/Books', Text:'Books'
+                        },{
+                            Link:'/Authors', Text:'Authors'}
+                             ],books: books});
+});
 
+bookRouter.route('/single')
+    .get(function(req,res){
+    res.send('Hello Single Book');
+});
+app.use('/Books',bookRouter);
 //var handlebars = require('express-handlebars');
 //app.engine('.hbs', handlebars({extname: '.hbs'}));
 //
@@ -23,7 +58,13 @@ app.set('view engine','ejs');
 //route
 app.get('/', function (req, res) {
     //res.send('hello world');
-    res.render('index',{title:'hello from render',list: ['a','b']});
+    res.render('index',{title:'hello from render',
+                        nav: [{
+                            Link:'/Books', Text:'Books'
+                        },{
+                            Link:'/Authors', Text:'Authors'}
+                             ] 
+                        });
 });
 
 app.get('/books', function (req, res) {
